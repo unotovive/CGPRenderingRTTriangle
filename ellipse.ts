868164d384402703dc700fa27916b9c880b83223
unotovive <tec.unotovive@gmail.com>
2018-11-29 16:05:40 +0900

    Fixes triangle.ts

--- ellipse.ts
+++ ellipse.ts
@@ -3,37 +3,37 @@
     get position(): THREE.Vector3 {
         return this.mpositioin;
     }
- 
+
     private msize: THREE.Vector3;
     get size(): THREE.Vector3{
         return this.msize;
     }
- 
+
     private mcolor: THREE.Color;
     get color(): THREE.Color {
         return this.mcolor;
     }
- 
-    private mka: number;
-    get ka(): number {
-        return this.mka;
+
+    private mia: number;
+    get ia(): number {
+        return this.mia;
     }
- 
-    private mkd: number;
-    get kd(): number {
-        return this.mkd;
+
+    private mid: number;
+    get id(): number {
+        return this.mid;
     }
- 
-    private mks: number;
-    get ks(): number {
-        return this.mks;
+
+    private mis: number;
+    get is(): number {
+        return this.mis;
     }
- 
+
     private mn: number;
     get n(): number {
         return this.mn;
     }
- 
+
     constructor(position: THREE.Vector3, size: THREE.Vector3, color: THREE.Color, ia: number, id: number, is: number, n: number)
     {
         this.mpositioin = new THREE.Vector3();
@@ -42,26 +42,80 @@
         this.msize.copy(size);
         this.mcolor = new THREE.Color;
         this.mcolor.copy(color);
-        this.mka = ia;
-        this.mkd = id;
-        this.mks = is;
+        this.mia = ia;
+        this.mid = id;
+        this.mis = is;
         this.mn = n;
+        console.log('test');
     }
- 
+
     calcT(e: THREE.Vector3, v: THREE.Vector3): number {
-        //第9回課題
-        return -1;
+        //課題はここを実装
+        var alpha, beta, gamma;//連立方程式から得られた t に関する二次方程式の係数
+        var D;//判別式
+        var K = 1;//二次形式⇒1 は楕円面を表す
+        var M = new THREE.Matrix4(); //4 行 4 列の係数行列
+        var R0d = new THREE.Vector3();
+        R0d.subVectors(e, this.position);
+        //e:カメラ（視点）位置 v:光線ベクトル（正規化済み）
+        //光線ベクトルが自身に衝突していればカメラからの距離を
+
+        M = M.scale(new THREE.Vector3(1 / (this.size.x * this.size.x), 1 / (this.size.y * this.size.y), 1 / (this.size.z * this.size.z)));
+        alpha = v.dot(v.clone().applyMatrix4(M));
+        beta = R0d.dot(v.clone().applyMatrix4(M));
+        gamma = R0d.dot(R0d.clone().applyMatrix4(M)) - K;
+        //判別式
+        D = beta * beta - alpha * gamma;
+        //衝突していなければ-1を返す
+        //console.log(D)
+        if(D < 0){
+            return -1;
+        } else {
+            return (-beta - Math.sqrt(D)) / alpha;
+        }
     }
- 
+
     calcNorm(p: THREE.Vector3): THREE.Vector3 {
         var mscale = new THREE.Matrix4().scale(new THREE.Vector3(1 / (this.size.x * this.size.x), 1 / (this.size.y * this.size.y), 1 / (this.size.z * this.size.z)));
-        var postop = p.clone().sub(this.position);
-        return postop.clone().applyMatrix4(mscale).normalize();
+        var postop = p;
+        postop.sub(this.position);
+        return postop.applyMatrix4(mscale).normalize();
     }
- 
-    calcShading(q: PointLight, p: THREE.Vector3, e: THREE.Vector3): THREE.Color    {
-        //第10回課題
-        return this.color;
-    }
-    
- }
+
+    calcShading(q: PointLight, p: THREE.Vector3, e: THREE.Vector3): THREE.Color {
+        //拡散反射光・鏡面反射光の計算に共通な項を計算する。⇒(4)
+
+        //交点と点光源との距離 r
+        var r = q.position.clone().sub(p).length();
+        //単位光源ベクトル L.
+        var L = q.position.clone().sub(p).divideScalar(r).normalize();
+        //単位法線ベクトル N
+        var N = this.calcNorm(p).normalize();
+        //単位視線ベクトル V
+        var V = p.clone().sub(e).normalize();
+        //環境光の強度 ia を求める。⇒(1)
+        var ia = this.ia
+        //光源からの光が交点に照射していなければ，
+        if(L.clone().dot(N) < 0) {
+            //交点は陰部分か影の内部かのいずれかであるので，
+            //反射光の強度は ia の値のみとなる。
+            //id, is はともに 0 となる。
+            var id = 0;
+            var is = 0;
+        } else {
+            var id = (this.id * q.ii / r**2) * N.clone().dot(L);
+            var R = (N.clone().multiplyScalar(2 * (N.clone().dot(L)))).sub(L).normalize();
+            var is = (this.is * q.ii / r ** 2) * R.clone().dot(V) ** this.n;
+           }
+        //光源からの光が交点に照射しているならば，
+        //交点は「陽」の部分にあるので，
+        //拡散反射光の強度 id を求める。⇒(2)
+        //単位正反射ベクトル R⇒(3)
+        //鏡面反射光の強度 is を求める。⇒(3)
+        //環境光，拡散反射光，鏡面反射光の和として，反射光の強度を求める。
+        var pR = this.color.r * ia + this.color.r * id + 255 * is;
+        var pG = this.color.g * ia + this.color.g * id + 255 * is;
+        var pB = this.color.b * ia + this.color.b * id + 255 * is;
+        return new THREE.Color(pR, pG, pB); //反射光を返す
+        }
+}
--- triangle.ts
+++ triangle.ts
@@ -10,17 +10,17 @@ class Triangle implements BaseShape {
     }
  
     private mka: number;
-    get ka(): number {
+    get ia(): number {
         return this.mka;
     }
  
     private mkd: number;
-    get kd(): number {
+    get id(): number {
         return this.mkd;
     }
  
     private mks: number;
-    get ks(): number {
+    get is(): number {
         return this.mks;
     }
  
@@ -58,8 +58,38 @@ class Triangle implements BaseShape {
     }
  
     calcT(e: THREE.Vector3, v: THREE.Vector3): number {
-        //今回の課題  
-        return -1;
+        const bunbo = this.normal.clone().dot(v);
+        const bunsi = this.normal.dot(this.verticies[0].clone().sub(e));
+        if (Math.abs(bunbo) < 0.000001) {
+            return -1; //平行なので交差していない，よって-1を返して終了する
+        } else {
+            //分子/分母で距離tを算出する
+            const t = bunsi / bunbo;
+            //e0，e1，e2を算出する
+            const e0 = this.verticies[0].clone().sub(this.verticies[1]);
+            const e1 = this.verticies[1].clone().sub(this.verticies[2]);
+            const e2 = this.verticies[2].clone().sub(this.verticies[0]);
+            //tから交点iを算出する
+            const i = e.clone().add(v.clone().multiplyScalar(t));
+            //iv0，iv1，iv2を算出する
+            const iv0 = this.verticies[0].clone().sub(i);
+            const iv1 = this.verticies[1].clone().sub(i);
+            const iv2 = this.verticies[2].clone().sub(i);
+            //cross0,cross1,cross2を算出し，
+            const cross0 = iv0.clone().cross(e0);
+            const cross1 = iv1.clone().cross(e1);
+            const cross2 = iv2.clone().cross(e2);
+            //cross0 * n
+            //cross1 * n
+            //cross2 * n すべて0より大きければ
+            if(cross0.clone().dot(this.normal) > 0 && cross1.clone().dot(this.normal) > 0 && cross2.clone().dot(this.normal) > 0) {
+                console.log(t)
+                return t;
+            } else {
+                return -1;
+            }
+        }
+
     }
  
     calcNorm(p: THREE.Vector3): THREE.Vector3 {
@@ -67,8 +97,40 @@ class Triangle implements BaseShape {
     }
  
     calcShading(q: PointLight, p: THREE.Vector3, e: THREE.Vector3): THREE.Color {
-        //第10回課題
-        return this.color;
+        //拡散反射光・鏡面反射光の計算に共通な項を計算する。⇒(4)
+
+        //交点と点光源との距離 r
+        var r = q.position.clone().sub(p).length();
+        //単位光源ベクトル L.
+        var L = q.position.clone().sub(p).divideScalar(r).normalize();
+        //単位法線ベクトル N
+        var N = this.calcNorm(p).normalize();
+        //単位視線ベクトル V
+        var V = p.clone().sub(e).normalize();
+        //環境光の強度 ia を求める。⇒(1)
+        var ia = this.ia
+        //光源からの光が交点に照射していなければ，
+        if(L.clone().dot(N) < 0) {
+            //交点は陰部分か影の内部かのいずれかであるので，
+            //反射光の強度は ia の値のみとなる。
+            //id, is はともに 0 となる。
+            var id = 0;
+            var is = 0;
+        } else {
+            var id = (this.id * q.ii / r**2) * N.clone().dot(L);
+            var R = (N.clone().multiplyScalar(2 * (N.clone().dot(L)))).sub(L).normalize();
+            var is = (this.is * q.ii / r ** 2) * R.clone().dot(V) ** this.n;
+           }
+        //光源からの光が交点に照射しているならば，
+        //交点は「陽」の部分にあるので，
+        //拡散反射光の強度 id を求める。⇒(2)
+        //単位正反射ベクトル R⇒(3)
+        //鏡面反射光の強度 is を求める。⇒(3)
+        //環境光，拡散反射光，鏡面反射光の和として，反射光の強度を求める。
+        var pR = this.color.r * ia + this.color.r * id + 255 * is;
+        var pG = this.color.g * ia + this.color.g * id + 255 * is;
+        var pB = this.color.b * ia + this.color.b * id + 255 * is;
+        return new THREE.Color(pR, pG, pB); //反射光を返す
     }
     
  }
